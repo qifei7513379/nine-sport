7826aebdb2ef02f9d1944ef3f0b7f885c2c936dc
qifei <1804219567@qq.com>
2017-09-20 17:52:19 +0800

    修改

--- nine-sport-mobile/js/main.js
+++ nine-sport-mobile/js/main.js
@@ -1,15 +1,25 @@
 
 //index轮播01
-$(function(){
-	$('.flexslider').flexslider({
-		directionNav: true,
-		pauseOnAction: false,
-		slideshowSpeed: 1000, //展示时间间隔ms
-		animationSpeed: 1000, //滚动时间ms
-		touch: true, //是否支持触屏滑动
-		pauseOnHover:true     //鼠标移动上面去停止
-	});
-});
+
+$(document).ready(function () {
+	
+  var mySwiper = new Swiper ('#swiper1', {
+//  direction : 'vertical',
+    loop: true,
+    autoplay:2000,
+    autoplayDisableOnInteraction : false,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+//  nextButton: '.swiper-button-next',
+//  prevButton: '.swiper-button-prev',
+    
+    // 如果需要滚动条
+//  scrollbar: '.swiper-scrollbar',
+  })
+  
+})
 
 
 
@@ -18,52 +28,26 @@ $(function(){
 
 /*********************/
 
+$(document).ready(function () {
+	
+  var mySwiper = new Swiper ('#swiper2', {
+//  direction : 'vertical',
+    loop: true,
+    autoplay:2000,
+    autoplayDisableOnInteraction : false,
+    // 如果需要分页器
+//  pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+    // 如果需要滚动条
+//  scrollbar: '.swiper-scrollbar',
+  })
+  
+})
 
-/*******************************/
-$(function() {
-	var _visible = 1;
-	var $pagers = $('#pager2 a');
-	var _onBefore = function() {
-		$(this).find('img').stop().fadeTo( 300, 1 );
-		$pagers.removeClass( 'selected' );
-	};
-
-	$('#carousel2').carouFredSel({
-		items: _visible,
-		width: '100%',
-		auto: true,
-		scroll: {
-			duration: 750
-		},
-		prev: {
-			button: '#prev2',
-			items: 1,
-			onBefore: _onBefore
-		},
-		next: {
-			button: '#next2',
-			items: 1,
-			onBefore: _onBefore
-		},
-	});
-
-	$pagers.click(function( e ) {
-		e.preventDefault();
-		
-		var group = $(this).attr( 'href' ).slice( 1 );
-		var slides = $('#carousel2 div.' + group);
-		var deviation = Math.floor( ( _visible - slides.length ) / 2 );
-		if ( deviation < 0 ) {
-			deviation = 0;
-		}
-
-		$('#carousel2').trigger( 'slideTo', [ $('#' + group), -deviation ] );
-		$('#carousel2 div img').stop().fadeTo( 300, 0.3 );
-		slides.find('img').stop().fadeTo( 300, 1 );
-
-		$(this).addClass( 'selected' );
-	});
-});
 /*********************************/
 
 
@@ -75,12 +59,7 @@ $(function() {
  $(window).scroll(function(){
      var sc=$(window).scrollTop();
      var rwidth=$(window).width()
-         if(sc>0){
-    $("#goTopBtn").css("display","block");
-    $("#goTopBtn").css("right",0+"px")
-    }else{
-   $("#goTopBtn").css("display","none");
-    }
+        
     })
 /****/
    $("#goTopBtn").click(function(){
@@ -88,66 +67,6 @@ $(function() {
    $('body,html').animate({scrollTop:0},500);
     })  
     
-
-
-/*
-*placeholder兼容ie9以下 author： add 2017-9-13
-*/
-$(function(){  
- 
-  //判断浏览器是否支持placeholder属性
-  supportPlaceholder='placeholder'in document.createElement('input'),
- 
-  placeholder=function(input){
- 
-    var text = input.attr('placeholder'),
-    defaultValue = input.defaultValue;
- 
-    if(!defaultValue){
- 
-      input.val(text).addClass("phcolor");
-    }
- 
-    input.focus(function(){
- 
-      if(input.val() == text){
-   
-        $(this).val("");
-      }
-    });
- 
-  
-    input.blur(function(){
- 
-      if(input.val() == ""){
-       
-        $(this).val(text).addClass("phcolor");
-      }
-    });
- 
-    //输入的字符不为灰色
-    input.keydown(function(){
-  
-      $(this).removeClass("phcolor");
-    });
-  };
- 
-  //当浏览器不支持placeholder属性时，调用placeholder函数
-  if(!supportPlaceholder){
- 
-    $('input').each(function(){
- 
-      text = $(this).attr("placeholder");
- 
-      if($(this).attr("type") == "text"){
- 
-        placeholder($(this));
-      }
-    });
-  }
- 
-});
-
 
 
 //侧边栏
@@ -189,3 +108,27 @@ $(function(){
 		})
 	}
 }(window.jQuery);
+
+
+
+//点击弹出层
+
+$(".big-link").on("click",function(){
+	
+	
+		$("#myModal").fadeIn("slow")
+		$(".reveal-modal-bg").fadeIn("slow")
+	$('.reveal-modal-bg').bind("touchmove",function(e){
+                e.preventDefault();
+     });	
+
+})
+
+$(".close-reveal-modal").on("click",function(){
+	
+		$("#myModal").fadeOut("slow")
+		$(".reveal-modal-bg").fadeOut("slow")
+	
+	
+})
+
